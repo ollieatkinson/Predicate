9fa680ad0c897608f3f58b3c46a3751c7276a38e
Oliver Atkinson <oliveratkinson@me.com>
2018-12-05 16:52:07 +0000

    Add travis.yml to support CI
    - Linux 14.04
    - macOS using Xcode 10
    - executes `swift test`

--- Package.swift
+++ Package.swift
@@ -6,10 +6,10 @@ import PackageDescription
 let package = Package(
     name: "Predicate",
     products: [
-        .library(name: "Predicate", targets: ["Predicate"]),
+        .library(name: "Predicate", targets: [ "Predicate" ]),
     ],
     targets: [
-        .target(name: "Predicate", dependencies: []),
+        .target(name: "Predicate", dependencies: [ ]),
         .testTarget(name: "PredicateTests", dependencies: [ "Predicate" ]),
     ]
 )
